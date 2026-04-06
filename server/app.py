b759c31ce184a7e639920ed690f9dd8421919157
sakshi <rrsakshikumari@gmail.com>
2026-04-06 13:40:36 +0530

    updated project last task

--- server/app.py
+++ server/app.py
@@ -1,4 +1,10 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from environment import app
 
+def main():
+    app.run(host='0.0.0.0', port=7860, debug=False)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7860)
+    main()
